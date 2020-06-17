6d432ac6a320d300d638a39e6129bb474b85cfe5
nikolasmelui <slak@samaradom.ru>
2020-06-17 17:25:09 +0400

    Move out all of the client logic from the main .go file

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"encoding/xml"
-	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"time"
 
 	"github.com/nikolasmelui/go-ecommerce-data-transfer-microservice/cache"
@@ -16,42 +11,13 @@ import (
 	"github.com/nikolasmelui/go-ecommerce-data-transfer-microservice/source"
 )
 
-type errorResponse struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-}
-
 func main() {
-	req, err := http.NewRequest("GET", source.ProductsURL, nil)
-	if err != nil {
-		log.Fatalf("Error: %v", err.Error())
-	}
 
-	req.Header.Set("Content-Type", "application/xml")
-	req.Header.Set("Accept", "application/xml")
-	req.SetBasicAuth(cconfig.Config.SourceBasicAuthLogin, cconfig.Config.SourceBasicAuthPassword)
+	sourceClient := source.NewClient()
 
-	client := &http.Client{
-		Timeout: time.Minute,
-	}
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Error: %v", err.Error())
-	}
-
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		var errRes errorResponse
-		if err := json.NewDecoder(res.Body).Decode(&errRes); err == nil {
-			log.Fatal(errors.New(errRes.Message))
-		}
-		log.Fatal(fmt.Errorf("Unknown error, status code: %d", res.StatusCode))
-	}
-
-	body, _ := ioutil.ReadAll(res.Body)
 	var productsResponse source.ProductsResponse
-	err = xml.Unmarshal(body, &productsResponse)
+
+	err := sourceClient.GetData("/products", &productsResponse)
 	if err != nil {
 		log.Fatalf("Error: %v", err.Error())
 	}
