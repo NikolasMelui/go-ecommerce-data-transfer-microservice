a3f3556f270597cff4b4ee982057a0ccc5fc72c6
nikolasmelui <slak@samaradom.ru>
2020-06-17 20:36:23 +0400

    Refactor the cache package Add product.go file with product logic

---
+++ cache/product.go
@@ -0,0 +1,26 @@
+package cache
+
+import (
+	"crypto/sha256"
+	"fmt"
+
+	"github.com/nikolasmelui/go-ecommerce-data-transfer-microservice/entity"
+)
+
+// ProductCache ...
+type ProductCache struct {
+	Data entity.Product
+	Hash string
+}
+
+// SetHash makes the instance sha256-hash and set it in the instance Hash field
+func (productCache *ProductCache) SetHash() {
+	hash := sha256.New()
+	hash.Write([]byte(fmt.Sprintf("%v", productCache.Data)))
+	productCache.Hash = fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+// GetHash returns the instance value of the hash field
+func (productCache *ProductCache) GetHash() string {
+	return productCache.Hash
+}
